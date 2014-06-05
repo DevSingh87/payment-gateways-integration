a7c5f259cde7f17f97370706b14e34effdbcb6b8
techiedev1987 <techiedev1987@gmail.com>
2014-06-05 12:26:17 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Paymentgateways::Application.config.secret_key_base = '12a71868eb181c10b3ac23f81ddcc4f2ff2fc618f217c2adf518190eee43026bc8041ff52e6f8f3fb59d79ba3fbb66a49893ea1e6b63142d358035705a2a66d1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Paymentgateways::Application.config.session_store :cookie_store, key: '_paymentgateways_session'
